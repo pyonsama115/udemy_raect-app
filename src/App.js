7a80743d2e1e79fe6ecd090066409aa062f3d082
pyon <pyonsama115@gmail.com>
2021-02-12 00:14:30 +0900

    props

--- src/App.js
+++ src/App.js
@@ -7,15 +7,30 @@ function App() {
 
   // return <input type="text" onChange={() => {console.log("clicked!")}}/>
 
+  const profiles = [
+    {name :"ぴょん", age: 15},
+    {name :"うーろん", age: 15},
+    {name :"noname" },
+  ]
   return (
     <React.Fragment>
-      <div>
-        a
-      </div>
-      <div>
-        sss
-      </div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
+      <User name={"Pyon"} age={15}/>
+      <User name={"うーろん"} age={37}/>
     </React.Fragment>
   )
 }
+
+const User = (props) => {
+  return <div>Hi {props.name} あなたは{props.age}歳ですね！</div>
+}
+
+User.defaultProps = {
+  age :99
+}
+
 export default App;
