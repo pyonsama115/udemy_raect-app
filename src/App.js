ba9df29ab1be4720d34f6a9f5eef7da806580535
pyon <pyonsama115@gmail.com>
2021-02-11 23:43:40 +0900

    reactFlagment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 function App() {
-  return <h1>hello,world</h1>;
+  // const greeting = "Hi Tom!";
+  // const dom =<h1 className="foo">{greeting}</h1>
+  // return dom;
+
+  // return <input type="text" onChange={() => {console.log("clicked!")}}/>
+
+  return (
+    <React.Fragment>
+      <div>
+        a
+      </div>
+      <div>
+        sss
+      </div>
+    </React.Fragment>
+  )
 }
 export default App;
